9b8bc696865b151cc2eb98169f07c2e569356e66
robertomahl <75228333+robertomahl@users.noreply.github.com>
2022-06-06 16:31:23 -0300

    Example code

---
+++ trabalho 2.pl
@@ -0,0 +1,28 @@
+concluiuEnsinoFundamental(julia).
+concluiuEnsinoFundamental(giovanna).
+concluiuEnsinoFundamental(andre).
+concluiuEnsinoFundamental(gabriel).
+concluiuEnsinoFundamental(fernanda).
+concluiuEnsinoFundamental(pedro).
+
+concluiuEnsinoMedio(julia).
+concluiuEnsinoMedio(pedro).
+concluiuEnsinoMedio(fernanda).
+concluiuEnsinoMedio(andre).
+
+nota(julia,760).
+nota(andre,550).
+nota(fernanda,800).
+nota(pedro,720.74).
+nota(giovanna,721).
+nota(gabriel,430).
+
+acimaNotaCorte(X) :- nota(X, Y), Y >= 720.75.
+   
+podeEntrarEnsinoSuperior(X) :-
+    concluiuEnsinoFundamental(X),
+    concluiuEnsinoMedio(X),
+    acimaNotaCorte(X).
+    
+    
+
